5b0308baad21dae9f5490a3d1f0a67f2048068bb
Ravi01459 <120428354+Ravi01459@users.noreply.github.com>
2023-02-01 21:08:13 -0500

    Update varibles.tf

--- varibles.tf
+++ varibles.tf
@@ -1,5 +1,5 @@
 variable "rgname" {
-  default     = "RG2"
+  default     = "RG12"
   type        = string
   description = "this is RG name"
 }
@@ -7,32 +7,4 @@ variable "loc" {
   default     = "East US"
   type        = string
   description = "this is RG Location"
-}
-variable "Vnetname" {
-  default     = "VNET2"
-  type        = string
-  description = "this is disk name"
-
-}
-variable "address_space" {
-  type    = list(any)
-  default = ["10.0.0.0/16"]
-}
-variable "Subnetaddress" {
-  type    = list(any)
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-
-}
-
-variable "Subnetname" {
-  type    = list(any)
-  default = ["subnet1", "subnet2", "subnet3"]
-
-}
-variable "tags" {
-  type = map(any)
-  default = {
-    environment = "Production"
-  }
-
 }
